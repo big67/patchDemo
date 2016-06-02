124974471cb53a9e7083554dd776b4560d3cb772
big67 <1020431814@qq.com>
2016-06-02 16:01:48 +0800

    Initial Commit

---
+++ patchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  patchDemo
+//
+//  Created by a on 16/6/2.
+//  Copyright © 2016年 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ patchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  patchDemo
+//
+//  Created by a on 16/6/2.
+//  Copyright © 2016年 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
